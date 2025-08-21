7366636dc7167f531960fadf5e12e487c0786080
TheAustinMiller <themilleraustin@gmail.com>
2025-08-21 16:20:45 -0500

    Cleaned up, added images, added more ppl

--- script.js
+++ script.js
@@ -2,13 +2,43 @@
 const celebrities = {
     timCook: {
         name: "Tim Cook",
-        salary: 76400000,
+        salary: 74600000,
         elementId: "cookCounter"
     },
     joshAllen: {
         name: "Josh Allen",
         salary: 55000000,
         elementId: "allenCounter"
+    },
+    yourDoctor: {
+        name: "Your Doctor",
+        salary: 239200,
+        elementId: "doctorCounter"
+    },
+    leoDicap: {
+        name: "Leonardo Dicaprio",
+        salary: 30000000,
+        elementId: "dicapCounter"
+    },
+    donaldTrump: {
+        name: "Donald Trump",
+        salary: 400000,
+        elementId: "trumpCounter"
+    },
+    crisRonaldo: {
+        name: "Cristiano Ronaldo",
+        salary: 260000000,
+        elementId: "crisCounter"
+    },
+    mrBeast: {
+        name: "Mr. Beast",
+        salary: 85000000,
+        elementId: "beastCounter"
+    },
+    loganPaul: {
+        name: "Logan Paul",
+        salary: 8500000,
+        elementId: "paulCounter"
     }
 };
 
@@ -37,3 +67,9 @@ function createCounter(celebrityKey) {
 // Initialize counters
 createCounter('timCook');
 createCounter('joshAllen');
+createCounter('leoDicap');
+createCounter('yourDoctor');
+createCounter('donaldTrump');
+createCounter('crisRonaldo');
+createCounter('mrBeast');
+createCounter('loganPaul');
